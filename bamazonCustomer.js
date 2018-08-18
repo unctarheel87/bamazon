88a27b12675daad5f40d4bd8692443b0edb716f3
John Tillman <john.w.tillman@gmail.com>
2018-08-18 10:04:18 -0400

    add product name to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,25 +9,35 @@ const connection = mysql.createConnection({
   multipleStatements: true
 });
 
-inquirer
-  .prompt([
-    {
-      name: 'product_id',
-      message: 'What is the id of the product you wish to buy?' 
-    },
-    {
-      name: 'units',
-      message: 'How many units would you like to buy?' 
-    },
-  ])
-  .then(function(answers) {
-    const id = answers.product_id;
-    const units = parseFloat(answers.units);
-    checkInventory(id, units);
-  });
+connection.connect();
+connection.query("SELECT * FROM products", function (error, results, fields) {
+  if (error) throw error;
+  console.log("");
+  console.table(results);
+  prompt()
+}); 
+
+
+function prompt() {
+  inquirer
+    .prompt([
+      {
+        name: 'product_id',
+        message: 'What is the id of the product you wish to buy?' 
+      },
+      {
+        name: 'units',
+        message: 'How many units would you like to buy?' 
+      },
+    ])
+    .then(function(answers) {
+      const id = answers.product_id;
+      const units = parseFloat(answers.units);
+      checkInventory(id, units);
+    });
+}  
  
 function checkInventory(id, units) {
-  connection.connect();
   const query = `SELECT stock_quantity FROM products 
                  WHERE item_id = ${id};`;
   connection.query(query, function (error, results, fields) {
@@ -44,12 +54,13 @@ function checkInventory(id, units) {
 function updateProducts(id, units) {
   const query =  `UPDATE products 
                   SET stock_quantity = stock_quantity - ${units}
-                  WHERE item_id =  ${id}; 
-                  SELECT cost FROM products 
+                  WHERE item_id = ${id}; 
+                  SELECT * FROM products 
                   WHERE item_id = ${id};`  
   connection.query(query, function (error, results, fields) {
     if (error) throw error;
-    console.log(`Your purchase cost $${results[1][0].cost * units}.`);
+    const product = results[1][0];
+    console.log(`\nYour purchase of ${product.product_name} comes to $${product.cost * units}.\n`);
     connection.end();
   }); 
 }
